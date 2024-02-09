f44faa1eb3a5a03c999bf1287b9c308be67cf023
root <root@LAPTOP-BJSQD76O.localdomain>
2024-02-10 00:36:30 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import hydra
+from hydra.utils import instantiate
+import torch
+import wandb
+from modeling.training import generate_samples, train_epoch
+
+@hydra.main(config_path=".", config_name="main_config")
+def main(clf):
+    clf['device'] = "cuda" if torch.cuda.is_available() else "cpu"
+    ddpm = instantiate(clf['DiffusionModel']).to(clf['device'])
+
+    train_transforms = instantiate(clf['Augmentations'])
+    dataset = instantiate(clf['CIFAR10'], transform=train_transforms)
+
+    dataloader = instantiate(clf['DataLoader'], dataset)
+    optim = instantiate(clf['optimizer'], ddpm.parameters())
+    
+    wandb.login()
+    wandb.init(project='hw_1', name=clf.name, config=clf)
+
+    
+    for i in range(clf['num_epochs']):
+        train_epoch(ddpm, dataloader, optim, clf['device'])
+        generate_samples(ddpm, clf['device'], f"samples/{i:02d}.png")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_pipeline.py
@@ -0,0 +1,67 @@
+import os
+
+import pytest
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, ToTensor, Normalize
+from torchvision.datasets import CIFAR10
+
+from modeling.diffusion import DiffusionModel
+from modeling.training import train_step, train_epoch, generate_samples
+from modeling.unet import UnetModel
+
+
+@pytest.fixture
+def train_dataset():
+    transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    dataset = CIFAR10(
+        "./data",
+        train=True,
+        download=True,
+        transform=transforms,
+    )
+    return dataset
+
+
+@pytest.mark.parametrize(["device"], [["cpu"], ["cuda"]])
+def test_train_on_one_batch(device, train_dataset):
+    # note: you should not need to increase the threshold or change the hyperparameters
+    ddpm = DiffusionModel(
+        eps_model=UnetModel(3, 3, hidden_size=32),
+        betas=(1e-4, 0.02),
+        num_timesteps=1000,
+    )
+    ddpm = ddpm.to(device) # fix 5, кажется, что так не работает
+
+    optim = torch.optim.Adam(ddpm.parameters(), lr=5e-4)
+    dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+
+    x, _ = next(iter(dataloader))
+    loss = None
+    for i in range(50):
+        loss = train_step(ddpm, x, optim, device)
+    assert loss < 0.5
+
+@pytest.mark.parametrize(["device"], [["cpu"], ["cuda"]])
+def test_training(device, train_dataset):
+    # fix 6 нагло копируем с test_train_on_one_batch
+    # note: implement and test a complete training procedure (including sampling)
+    
+    path = 'test_dir'
+    if not os.path.exists(path):
+        os.mkdir(path)
+    
+    ddpm = DiffusionModel(
+        eps_model=UnetModel(3, 3, hidden_size=32),
+        betas=(1e-4, 0.02),
+        num_timesteps=1000,
+    )
+    ddpm = ddpm.to(device)
+
+    optim = torch.optim.Adam(ddpm.parameters(), lr=5e-4)
+    dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+
+    
+    for i in range(5):
+        train_epoch(ddpm, dataloader, optim, device)
+        generate_samples(ddpm, device, f"{path}/i.png")
