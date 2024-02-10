86bd6a5a28f5d6f46927329ee9e5e7b519be8e15
root <root@LAPTOP-BJSQD76O.localdomain>
2024-02-10 23:51:27 +0300

    Fix

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ def main(clf):
 
     generate_samples(ddpm, clf['device'], f"{path}/0.png")
     noise = torch.randn((8, 3, 32, 32), device=clf['device'])
-    wandb.log({"Generate Images": wandb.Image(noise)})
+    wandb.log({"Generate Images": wandb.Image(noise)}, step=0)
     for i in range(clf['num_epochs']):
         images = train_epoch(ddpm, dataloader, optim, clf['device'], i, clf['logging_policy'])
         generate_samples(ddpm, clf['device'], f"{path}/{i+1:02d}.png")
         result = generate_samples(ddpm, clf['device'], f"{path}/{i+1:02d}.png", noise)
-        wandb.log({"Inputs": wandb.Image(images)})
-        wandb.log({"Generate Images": wandb.Image(result)})
+        wandb.log({"Inputs": wandb.Image(images)}, step=(i+1)*len(dataloader))
+        wandb.log({"Generate Images": wandb.Image(result)}, step=(i+1)*len(dataloader))
 
 
 if __name__ == "__main__":
